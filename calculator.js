b7137ce32d56ad1a6a4d837723f66fcea74da8fe
HG3182 <hrushigawade8@gmail.com>
2024-03-14 16:22:10 +0530

    added history button to view last calculation

--- calculator.js
+++ calculator.js
@@ -1,8 +1,25 @@
 
 const display = document.getElementById("display");
+var valueComputed = false;
+const history = [];
+
+
+function appendToDisplayNumber(input){
+    if(valueComputed ){
+        display.value = "";
+        valueComputed = false;
+        
+    }
+    
+    display.value += input;
+    history.push(display.value);
+
+}
 
 function appendToDisplay(input){
+    valueComputed = false;
     display.value += input;
+    history.push(display.value);
 }
 
 function clearAll(){
@@ -14,9 +31,21 @@ function calculate(){
 
     try{
         display.value = eval(display.value);
+        history.push(display.value);
+
     }
     catch{
         display.value = "Error"
     }
+    valueComputed = true;
+}
+
+function viewHistory(){
+var len=history.length-1;
+var toDipsplay;
+
+toDipsplay=history[len-1] + "=" + history[len];
+    display.value=toDipsplay;
 
 }
+
